fecf0059ba83b63499356350642281affe0fab31
Weijian Yang <weijian.yang@rea-group.com>
2023-01-08 10:42:00 +0800

    move retrieve variable in steps

--- demo.sh
+++ demo.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-echo "time=${{ steps.hello-goudan.outputs.time }}"
+echo "time=${1}"
 docker volume create demo
 docker build -t test-action .
 docker run --rm --mount src=demo,dst=/demo test-action
