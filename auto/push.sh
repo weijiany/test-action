e8dd22c0b9d779f58e147e143e0f1d7ff40a3655
Weijian Yang <weijian.yang@rea-group.com>
2023-01-29 20:41:37 +0800

    login ecr in github action.

--- auto/push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-image="494526681395.dkr.ecr.ap-east-1.amazonaws.com/goudan-test:${GITHUB_RUN_ID}"
-
-docker pull nginx:1.23.2
-docker tag nginx:1.23.2 "${image}"
-docker push "${image}"
